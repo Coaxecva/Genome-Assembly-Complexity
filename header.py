2800a9fc3815c3c8d22796d37a3cd976bd09d5cf
Coaxecva <qmtran@memphis.edu>
2015-01-21 12:52:29 -0600

    update header.py

--- header.py
+++ header.py
@@ -6,6 +6,8 @@ mypath = sys.argv[1]
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 for f in onlyfiles:
+	print(f + "\n")
+	#raw_input()
 	str = mypath + f
 	cmd = "head -n 1 " + str
 	#print genome name
