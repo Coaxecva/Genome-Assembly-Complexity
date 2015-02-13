e9e6e8ce84ffcc3385619d92abb748d403d8bb0f
Coaxecva <qmtran@memphis.edu>
2015-02-13 10:32:50 -0600

    update header.py

--- header.py
+++ header.py
@@ -2,13 +2,20 @@ import os, sys, string
 from os import listdir
 from os.path import isfile, join
 
+complexity_path = " ../sequence-complexity/usage/compute_complexity.go "
+
 mypath = sys.argv[1]
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
-for f in onlyfiles:
-	print(f + "\n")
-	#raw_input()
+for f in onlyfiles:	
 	str = mypath + f
-	cmd = "head -n 1 " + str
+	#print(f)
+	#raw_input()
+
+	#cmd = "head -n 1 " + str
+	#print genome name
+	#os.system(cmd)
+
+	cmd = "go run" + complexity_path + str
 	#print genome name
 	os.system(cmd)
